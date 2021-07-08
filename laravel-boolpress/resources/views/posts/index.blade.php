9e7d66839b34f26ff0a8da257de35fb80ec65cb0
Giulia-M <giuliamaggio16@gmail.com>
2021-07-08 21:40:49 +0200

    img nei post pubblici

--- laravel-boolpress/resources/views/posts/index.blade.php
+++ laravel-boolpress/resources/views/posts/index.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 <div class="row justify-content-center">
   <div class="col-12">
-
+   
     @foreach ($posts as $post)
-    
+      <div>
+        <img src="{{ $post->cover_url ? asset('storage/' . $post->cover_url) : 'https://www.linga.org/site/photos/Largnewsimages/image-not-found.png'}}" class="img-fluid rounded-start" alt="..." >
+      </div>
       <h5 class="card-title">{{ $post->title }}</h5>
       <p class="card-text">{{ substr($post->content, 0, 80) }}</p>
       <p class="card-text"><small class="text-muted">{{ $post->updated_at }}</small></p>
